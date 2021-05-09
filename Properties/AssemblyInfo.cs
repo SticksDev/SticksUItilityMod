3ddc49e31238b434bf3d739d59718ac16ca91f59
Sticks <tsommers150@gmail.com>
2021-05-09 11:17:08 -0400

    Update AssemblyVersion, and AssemblyFileVersion

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using SticksUItilityMod;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
 
 // Melon Loader Information
 [assembly: MelonInfo(typeof(MainMod), "SticksUitilityMod", "0.0.1", "sticks#6436")]
